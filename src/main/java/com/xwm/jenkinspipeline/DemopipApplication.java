ffccb543e76cecf67688f31dfa51aab3bf9c10c1
xuweiming <whxuwm@cn.ibm.com>
2021-01-11 16:19:32 +0800

    init

---
+++ src/main/java/com/xwm/jenkinspipeline/DemopipApplication.java
@@ -0,0 +1,13 @@
+package com.xwm.jenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xwm/jenkinspipeline/DemopipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xwm.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
